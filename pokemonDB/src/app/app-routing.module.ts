277ce2298ee8a1b7b47e1fe4290a8bacfc20b979
obeymyshinnyrod <miswah.md@gmail.com>
2021-06-10 18:23:45 +0530

    added previous/next buttons and actions

---
+++ pokemonDB/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
+const routes: Routes = [
+  { path: 'pokemon', component: PokemonListComponent },
+
+  { path: 'pokemon/:id', component: PokemonDetailsComponent },
+
+  {
+    path: '',
+    redirectTo: 'pokemon',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ pokemonDB/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+
+@NgModule({
+  declarations: [AppComponent, PokemonListComponent, PokemonDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, RouterModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
