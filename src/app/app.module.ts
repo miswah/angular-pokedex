cbc661f93f745f8953a5aa6d113d6227f2422a1a
obeymyshinnyrod <miswah.md@gmail.com>
2021-10-17 23:59:37 +0530

    (redo):Fixed the structure of the project close #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { ViewComponent } from './components/view/view.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ViewComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
