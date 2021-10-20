fd284adcb764be1341099529e43ba14c25d802df
obeymyshinnyrod <miswah.md@gmail.com>
2021-10-20 23:43:26 +0530

    Added documentation via compodoc

--- src/app/services/pokedex-management.service.ts
+++ src/app/services/pokedex-management.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokedexManagementService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  //fetching pokemon 3 at a time
+  getPokemons() {
+    return this.http.get(environment.pokeAPI + `pokemon?limit=3&offset=0`);
+  }
+
+  getNextPokemons(next: string) {
+    return this.http.get(next);
+  }
 }
