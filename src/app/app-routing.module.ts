2b68262cd34e45ad86d6a62915db5da2b8db85b4
obeymyshinnyrod <miswah.md@gmail.com>
2021-10-21 23:44:58 +0530

    (feat): added routing for all component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ViewComponent } from './components/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'view/:id', component: ViewComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
